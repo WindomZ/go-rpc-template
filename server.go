47aa638139537698127ea3b25f4aed6f47ea5a5f
WindomZ <zwdsdtc@gmail.com>
2016-05-24 13:12:59 +0800

    fix: server bugs

--- server.go
+++ server.go
@@ -23,7 +23,7 @@ type RpcServer struct {
 }
 
 func NewRpcServer(c RpcServerConfig, f RpcLogFunc) (*RpcServer, error) {
-	if c == nil {
+	if !c.Valid() {
 		return nil, ErrConfig
 	}
 	return &RpcServer{
@@ -34,59 +34,59 @@ func NewRpcServer(c RpcServerConfig, f RpcLogFunc) (*RpcServer, error) {
 	}, nil
 }
 
-func (c *RpcServer) IsRunning() bool {
-	return c.isRunning
+func (s *RpcServer) IsRunning() bool {
+	return s.isRunning
 }
 
-func (c *RpcServer) GetLinkAddress() string {
-	return fmt.Sprintf(":%v", c.config.Port)
+func (s *RpcServer) GetLinkAddress() string {
+	return fmt.Sprintf(":%v", s.config.Port)
 }
 
-func (c *RpcServer) log(str string, err error) {
-	if c.logFunc != nil {
-		c.logFunc(str, err)
+func (s *RpcServer) log(msg string, err error) {
+	if s.logFunc != nil {
+		s.logFunc(msg, err)
 	}
 }
 
-func (c *RpcServer) run() {
-	c.isRunning = true
-	c.log(fmt.Sprintf("gorpc: server running on: %v",
-		c.config.Port), nil)
+func (s *RpcServer) run() {
+	s.isRunning = true
+	s.log(fmt.Sprintf("gorpc: server running on: %v",
+		s.config.Port), nil)
 	if err := rpc.Register(new(RpcPing)); err != nil {
-		c.log(fmt.Sprintf("gorpc: server register error: %v", err), err)
-	} else if addr, err := net.ResolveTCPAddr("tcp", c.GetLinkAddress()); err != nil {
-		c.log(fmt.Sprintf("gorpc: server resolve address error: %v", err), err)
+		s.log(fmt.Sprintf("gorpc: server register error: %v", err), err)
+	} else if addr, err := net.ResolveTCPAddr("tcp", s.GetLinkAddress()); err != nil {
+		s.log(fmt.Sprintf("gorpc: server resolve address error: %v", err), err)
 	} else if listener, err := net.ListenTCP("tcp", addr); err != nil {
-		c.log(fmt.Sprintf("gorpc: server listen error: %v", err), err)
+		s.log(fmt.Sprintf("gorpc: server listen error: %v", err), err)
 	} else {
-		for c.IsRunning() {
+		for s.IsRunning() {
 			if conn, err := listener.Accept(); err != nil {
 				time.Sleep(time.Second)
 				continue
 			} else {
 				rpc.ServeConn(conn)
-				c.log(fmt.Sprintf("gorpc: server connected: %v",
+				s.log(fmt.Sprintf("gorpc: server connected: %v",
 					conn.RemoteAddr().String()), nil)
 			}
 		}
 	}
-	c.isRunning = false
-	c.log("gorpc: server close", nil)
+	s.isRunning = false
+	s.log("gorpc: server close", nil)
 }
 
-func (c *RpcServer) Start() {
-	if c.IsRunning() {
+func (s *RpcServer) Start() {
+	if s.IsRunning() {
 		return
 	}
-	c.mutex.Lock()
-	defer c.mutex.Unlock()
-	if !c.IsRunning() {
-		go c.run()
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+	if !s.IsRunning() {
+		go s.run()
 	}
 }
 
-func (c *RpcServer) Stop() {
-	c.mutex.Lock()
-	defer c.mutex.Unlock()
-	c.isRunning = false
+func (s *RpcServer) Stop() {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+	s.isRunning = false
 }
