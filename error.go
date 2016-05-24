786fea4d6727971b2b0ed5885a41f94d6cdda99c
WindomZ <zwdsdtc@gmail.com>
2016-05-24 12:12:36 +0800

    feat: new error

---
+++ error.go
@@ -0,0 +1,9 @@
+package gorpc
+
+import "errors"
+
+var (
+	ErrClient  error = errors.New("gorpc: Did not start the client")
+	ErrConnect       = errors.New("gorpc: No connected rpc server")
+	ErrConfig        = errors.New("gorpc: Invalid config")
+)
